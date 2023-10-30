acc71e66b82be5d88541c209a063245d25ca0f7e
oscar <oscar.m.chevalier@gmail.com>
2023-10-30 13:37:16 +0100

    recreated the architecture 3

---
+++ structures/decouplers.c
@@ -0,0 +1,46 @@
+#include "decouplers.h"
+
+#include <stdlib.h>
+
+#include "decoupler.h"
+#include "../reader.h"
+#include "utils.h"
+
+void free_decouplers(struct decouplers *d)
+{
+    for (size_t i = 0; i < NBR_DIAMETER; i++)
+    {
+        for (int j = 0; j < d->nbr[i]; j++)
+        {
+            free(d->elements[i][j]->name);
+            free(d->elements[i][j]);
+        }
+        free(d->elements[i]);
+    }
+    free(d->elements);
+    free(d->nbr);
+    free(d);
+}
+
+bool define_decouplers(struct datas *d)
+{
+    if (!d)
+        return false;
+    struct decouplers *decouplers = calloc(1, sizeof(struct decouplers));
+    if (!decouplers)
+        return false;
+    decouplers->elements = calloc(NBR_DIAMETER, sizeof(struct decoupler **));
+    if (!decouplers->elements)
+    {
+        free_decouplers(decouplers);
+        return false;
+    }
+    decouplers->nbr = calloc(NBR_DIAMETER, sizeof(int));
+    if (!decouplers->nbr)
+    {
+        free_decouplers(decouplers);
+        return false;
+    }
+    d->decouplers = decouplers;
+    return read_decouplers(d, "decouplers.csv") > 0;
+}
